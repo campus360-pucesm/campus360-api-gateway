c502d635fe990ef01e692b04d743ff9052e3c168
SrChaoz <mateorubencho@gmail.com>
2026-01-16 20:57:26 -0500

    Mergue

--- app/core/config.py
+++ app/core/config.py
@@ -3,9 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-RESERVAS_SERVICE_URL = os.getenv("RESERVAS_SERVICE_URL", "http://reservas-service:8001/api/v1/reservas")
-INCIDENCIAS_SERVICE_URL = os.getenv("INCIDENCIAS_SERVICE_URL", "http://incidencias-service:8002/api/v1/incidencias")
-AUTH_SERVICE_URL = os.getenv("AUTH_SERVICE_URL", "http://localhost:8000")
-SECRET_KEY = os.getenv("SECRET_KEY", "your-secret-key-here")
+RESERVAS_SERVICE_URL = os.getenv("RESERVAS_SERVICE_URL", "http://localhost:8001/api/v1/reservas")
+INCIDENCIAS_SERVICE_URL = os.getenv("INCIDENCIAS_SERVICE_URL", "http://localhost:8002/api/v1/incidencias")
+AUTH_SERVICE_URL = os.getenv("AUTH_SERVICE_URL", "http://localhost:8003")
+ATTENDANCE_SERVICE_URL = os.getenv("ATTENDANCE_SERVICE_URL", "http://localhost:8004")
+SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-campus360-key")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 
